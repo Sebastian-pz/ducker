32a15183e565797a4821dee690dcabafe1c0c582
Sebastian Perez <spezuluaga@gmail.com>
2023-01-13 16:18:51 -0500

    first steps, eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'space-before-function-paren': ['error', 'never'],
+    'comma-dangle': 'off',
+  },
+}
