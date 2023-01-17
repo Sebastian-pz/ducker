071a915ffd672378749e42653b68ef8cd83fb973
Sebastian Perez <spezuluaga@gmail.com>
2023-01-17 12:33:30 -0500

    code/ sass added

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router'
 import { Provider } from 'react-redux'
 import store from './App/store'
+import './styles/styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
