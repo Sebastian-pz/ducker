4d6c492ba185c0d433d9a97969f6a1762fc737b2
Matias Straface <matiasgstraface@gmail.com>
2023-01-18 17:57:37 -0300

    code/seacrchbar, home, login

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,21 +1,85 @@
 import './Home.modules.css'
 import { Link } from 'react-router-dom'
 import Logo from '../../Assets/Img/ducker-logo.png'
+import SearchBar from '../../Components/SearchBar/SearchBar'
+import { getUserById } from '../../Features/User/functions'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.user.completeUser)
+  let id = useSelector(state => state.user.userId)
+
+  useEffect(() => {
+    id = localStorage.getItem('auth')
+    dispatch(getUserById(id))
+  }, [dispatch, user, id])
+
   return (
     <div className='home-container'>
       <section className='section1'>
         <Link to='#'>
           <img src={Logo} alt='Logo' />
         </Link>
-        <h1>Hola Mundo! 1</h1>
+        <ul className='navList'>
+          <li>
+            <i className='bx bxs-home-circle'></i>
+            <a to={''}>Inicio</a>
+          </li>
+          <li>
+            <i className='bx bx-hash'></i>
+            <a href=''>Explorar</a>
+          </li>
+          <li>
+            <i className='bx bx-bell'></i>
+            <a href=''>Notificar</a>
+          </li>
+          <li>
+            <i className='bx bx-envelope'></i>
+            <a href=''>Mensajería</a>
+          </li>
+          <li>
+            <i className='bx bx-bookmark'></i>
+            <a href=''>Guardados</a>
+          </li>
+          <li>
+            <i className='bx bx-list-ul'></i>
+            <a href=''>Listas</a>
+          </li>
+          <li>
+            <i className='bx bx-user'></i>
+            <a href=''>Perfil</a>
+          </li>
+          <li className='dropdown'>
+            <i className='bx bx-cog'></i>
+            <a href='' className='dropbtn'>
+              {/* <a href='javascript:void(0)' className='dropbtn'> */}
+              Más opciones
+              {/* <div className='dropdown-content'>
+                <a href=''>Configuración y privacidad</a>
+                <a href=''>Centro de ayuda</a>
+              </div> */}
+            </a>
+          </li>
+        </ul>
+        <button>Cuackear</button>
+        <div className='container-icon-name-nick'>
+          <img src='' alt='' />
+          <div className='container-name-nick'>
+            <h4>{user && user.fullname}</h4>
+            <h5>{user && user.nickname}</h5>
+          </div>
+          <i className='bx bx-cog'></i>
+        </div>
       </section>
       <section className='section2'>
-        <h1>Hola Mundo! 2</h1>
+        <h1>{user && user.fullname}</h1>
       </section>
       <section className='section3'>
-        <h1>Hola Mundo! 3</h1>
+        <SearchBar className='searchabar' />
+        <div className='datosUsuario'></div>
+        <div className='tendencias'></div>
       </section>
     </div>
   )
