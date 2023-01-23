285c9014a4810931af8b076ce092a2137ad724a2
Sebastian Perez <spezuluaga@gmail.com>
2023-01-23 11:17:21 -0500

    structure - remove deafult picture and change folder

--- src/Components/Cuackear/Cuackear.jsx
+++ src/Components/Cuackear/Cuackear.jsx
@@ -1,13 +1,9 @@
 /* eslint-disable react/prop-types */
-import profilePicture from '../Cuack/profile.jpg'
-
 const Cuackear = () => {
-  // img = state
-
   return (
     <div className='cuackear-container'>
       <div className='cuackear-main'>
-        <img src={profilePicture} alt='profile-picture' htmlFor='cuackear' />
+        <img src={''} alt='profile-picture' htmlFor='cuackear' />
         <input type='text' name='cuackear' id='' autoComplete='off' />
       </div>
       <div className='display-flex-end'>
