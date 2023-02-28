b0c39a58f54074e1b74f5b430326b19856f5f465
Sebastian Perez <spezuluaga@gmail.com>
2023-02-28 11:53:27 -0500

    fix token problem on non logged user

--- src/Utils/user.js
+++ src/Utils/user.js
@@ -5,8 +5,6 @@ import { getCuacks } from '../Features/Cuack/cuackFunctions'
 
 const uri = process.env.REACT_APP_BACK_URL || 'http://localhost:3001'
 
-const userId = getUserID()
-
 const config = {
   headers: {
     Authorization: localStorage.getItem('Authorization'),
@@ -14,6 +12,7 @@ const config = {
 }
 
 export async function handleUserActions(dispatch, id, type) {
+  const userId = getUserID() || undefined
   try {
     const { data } = await axios.put(
       `${uri}/users/${type}/${userId}`,
